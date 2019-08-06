fbeba934bde92fc73214fde2c59321bee905bfca
Kevin Pleitez <pleitez95geova@gmail.com>
2019-08-05 20:11:30 -0600

    Se agrego el login como una ruta falta crear el authguard para la seguridad

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import {LoginComponent} from './login.component'
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  exports: [LoginComponent],
+  imports: [
+    RouterModule,
+    CommonModule
+  ]
+})
+export class LoginModule { }
